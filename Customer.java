6ed9f142548e753423ccf1920ecba894f29002d9
Niruni Imesha <88593456+NiruniImesha@users.noreply.github.com>
2021-10-07 21:57:42 +0530

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,15 +1,15 @@
 public class Customer {
     private int id;
-    private String name
-    private String email;
+    private String name;// Add semi-colon
+    private String email;// Add semi-colon
     private String phone;
     private String userName;
     private String password;
     
-	public Customer(int id, String name, String email, string phone, String userName, String password) {
+	public Customer(int id, String name, String email, String phone, String userName, String password) {
 		this.id = id;
 		this.name = name;
-		this.email = email
+		this.email = email;
 		this.phone = phone;
 		this.userName = userName;
 		this.password = password;
@@ -20,34 +20,34 @@ public class Customer {
 	}
 
 	public void setId(int id) {
-		this.Id = id
+		this.id = id;
 	}
 
-	public String getname() {
+	public String getName() {
 		return name;
 	}
 
-	public void getName(String name) {
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public String setEmail() {
+	public String getEmail() {
 		return email;
 	}
 
-	public void setemail(string email) {
+	public void setEmail(String email) {
 		this.email = email;
 	}
 
 	public String getPhone() {
-		return phone
+		return phone;
 	}
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
-	public String getuserName() {
+	public String getUserName() {
 		return userName;
 	}
 
