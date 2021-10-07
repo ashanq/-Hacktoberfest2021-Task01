cccec7e78e73c0a1bc3baf64945bb51b8960f348
ashanq <79820774+ashanq@users.noreply.github.com>
2021-10-07 21:42:28 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,68 @@
+public class Customer {
+    private int id;
+    private String name
+    private String email;
+    private String phone;
+    private String userName;
+    private String password;
+    
+	public Customer(int id, String name, String email, string phone, String userName, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email
+		this.phone = phone;
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.Id = id
+	}
+
+	public String getname() {
+		return name;
+	}
+
+	public void getName(String name) {
+		this.name = name;
+	}
+
+	public String setEmail() {
+		return email;
+	}
+
+	public void setemail(string email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getuserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+	
+    
+}
