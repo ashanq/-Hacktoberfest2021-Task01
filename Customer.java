ac50678e4e19134ec438bdc25336fa3a4f8fde5e
Kobigan Krishnananthan <84832594+Kobigan1223@users.noreply.github.com>
2021-10-10 17:41:09 +0530

    Customer.Java Updated

--- Customer.java
+++ Customer.java
@@ -1,11 +1,12 @@
 public class Customer {
     private int id;
     private String name
+    private String name;
     private String email;
     private String phone;
     private String userName;
     private String password;
-    
+
 	public Customer(int id, String name, String email, string phone, String userName, String password) {
 		this.id = id;
 		this.name = name;
@@ -13,56 +14,81 @@ public class Customer {
 		this.phone = phone;
 		this.userName = userName;
 		this.password = password;
+
+    public Customer(int id, String name, String email, String phone, String userName, String password) {
+
+	this.id = id;
+	this.name = name;
+	this.email = email;
+	this.phone = phone;
+	this.userName = userName;
+	this.password = password;
+
 	}
 
+	
 	public int getId() {
 		return id;
 	}
 
+	
 	public void setId(int id) {
 		this.Id = id
+		this.Id = id;
 	}
 
 	public String getname() {
+	
+	public String getName() {
 		return name;
 	}
 
 	public void getName(String name) {
+	
+	public void setName(String name) {
 		this.name = name;
 	}
 
 	public String setEmail() {
+	
+	public String getEmail() {
 		return email;
 	}
 
 	public void setemail(string email) {
+	
+	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	
 	public String getPhone() {
 		return phone
+		return phone;
 	}
 
+	
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
 	public String getuserName() {
+	
+	public String getUserName() {
 		return userName;
 	}
 
+	
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
 
+	
 	public String getPassword() {
 		return password;
 	}
 
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
-    
-	
-    
-}
